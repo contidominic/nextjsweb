cf4bc9371d7c9c02e4277d275acb57bbf03518bc
contidominico <dominicoclt@gmx.co.uk>
2021-04-07 20:28:04 +0100

    e

--- src/pageone/FeatBoxes1.js
+++ src/pageone/FeatBoxes1.js
@@ -1,14 +1,41 @@
- 
+import React, { useState , useEffect} from "react";
 import Image from 'next/image'
 
 export default function Featboxes1() {
+  const [Scr, SetScr] = useState (false);
+  
+
+  const HandleClick = () => {
+      var scrolled = document.scrollingElement.scrollTop;
+      scrolled > 500 ? SetScr(true) : null;
+      console.log("done")
+
+
+  }
+
+  useEffect(() => {
+      document.addEventListener("scroll", HandleClick);
+    }, []);
+
   return (
  
    <div className="imageC">
     <div className="confeatt">
      <div className="firstDiv">
      <div id ="test1" style={{ position: 'relative', width: "100%", height: '500px' }}>
-  
+
+     {Scr ?
+
+     <Image
+      alt="Mountains"
+      src="/spa.jpg"
+      layout="intrinsic"
+      width={700}
+      height={475}
+    />
+
+: null }
+     
       
     </div>
     </div>
